99cdbf32480e5298b4abe9503c2dca9c6708221d
tuierbufei <52435770@qq.com>
2016-06-22 18:14:46 +0800

    Update ScheduledTasks.java

--- src/main/java/hello/ScheduledTasks.java
+++ src/main/java/hello/ScheduledTasks.java
@@ -3,14 +3,11 @@ package hello;
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.http.client.ClientHttpRequest;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -25,6 +22,11 @@ public class ScheduledTasks {
 	
 	@Scheduled(fixedRate = 24*60*60*1000)
 	public void getFinancialReport() {
+//		SimpleClientHttpRequestFactory clientHttpRequestFactory = new SimpleClientHttpRequestFactory();
+//		Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress("127.0.0.1", 9999));
+//		clientHttpRequestFactory.setProxy(proxy);
+		
+//		RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory);
 		RestTemplate restTemplate = new RestTemplate();
 		try {
 			ClientHttpRequest request = restTemplate.getRequestFactory().createRequest(new URI("http://www.xueqiu.com"), HttpMethod.GET);
@@ -37,13 +39,26 @@ public class ScheduledTasks {
 					 token = temp.replace(cookiesKey + "=" , "");
 				 }
 			}
-//			//http://xueqiu.com/stock/f10/finmainindex.json?symbol=SH601988&page=2&size=4&_=1466526456836
-//			//http://xueqiu.com/stock/f10/incstatement.json?symbol=SZ002353&page=1&size=4&_=1466527285322
-//			restTemplate.getRequestFactory().createRequest(arg0, arg1)
-//			request = restTemplate.getRequestFactory().createRequest(new URI("http://xueqiu.com/stock/f10/incstatement.json?symbol=SZ002353&page=1&size=4&_=1466527285322"), HttpMethod.GET);
-			ResponseEntity<Object> obj = restTemplate.getForEntity("http://xueqiu.com/stock/f10/incstatement.json?symbol=SZ002353&page=1&size=1000&access_token=" + token, Object.class);
-			System.out.println(obj);
-			Object list = ((LinkedHashMap)obj.getBody()).get("list");
+			IncStatementList incStatementList = restTemplate.getForObject("http://xueqiu.com/stock/f10/incstatement.json?symbol=SZ002353&page=1&size=4&access_token=" + token, IncStatementList.class);
+			for (IncStatement incStatement : incStatementList.getList()) {
+				System.out.println(incStatement);
+			}
+			
+			BalSheetList balSheetList = restTemplate.getForObject("http://xueqiu.com/stock/f10/balsheet.json?symbol=SZ002353&page=1&size=4&access_token=" + token, BalSheetList.class);
+			for (BalSheet balSheet : balSheetList.getList()) {
+				System.out.println(balSheet);
+			}
+			
+			CFStatementList cfStatementList = restTemplate.getForObject("http://xueqiu.com/stock/f10/cfstatement.json?symbol=SZ002353&page=1&size=4&access_token=" + token, CFStatementList.class);
+			for (CFStatement cFStatement : cfStatementList.getList()) {
+				System.out.println(cFStatement);
+			}
+			
+			FinMainIndexList finMainIndexList = restTemplate.getForObject("http://xueqiu.com/stock/f10/finmainindex.json?symbol=SZ002353&page=1&size=4&access_token=" + token, FinMainIndexList.class);
+			for (FinMainIndex finMainIndex : finMainIndexList.getList()) {
+				System.out.println(finMainIndex);
+			}
+			
 		} catch (IOException | URISyntaxException e) {
 			logger.error(e.getMessage(), e);
 		}
