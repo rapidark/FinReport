cf742e3b33d6978d3e10b89d7ba17b44efbaad7e
tuierbufei <52435770@qq.com>
2016-06-22 18:13:55 +0800

    Create BalSheetList.java

---
+++ src/main/java/hello/BalSheetList.java
@@ -0,0 +1,25 @@
+package hello;
+
+import java.util.List;
+
+public class BalSheetList {
+	private int comptype;
+
+	private List<BalSheet> list;
+
+	public int getComptype() {
+		return comptype;
+	}
+
+	public void setComptype(int comptype) {
+		this.comptype = comptype;
+	}
+
+	public List<BalSheet> getList() {
+		return list;
+	}
+
+	public void setList(List<BalSheet> list) {
+		this.list = list;
+	}
+}
