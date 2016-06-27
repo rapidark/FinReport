85981546de15de859ea851973e47ef36007cc841
tuierbufei <52435770@qq.com>
2016-06-27 18:39:37 +0800

    Update FinStatementDaoImpl.java

--- src/main/java/com/finreport/dao/FinStatementDaoImpl.java
+++ src/main/java/com/finreport/dao/FinStatementDaoImpl.java
@@ -1,7 +1,12 @@
 package com.finreport.dao;
 
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.finreport.mapper.BalSheetMapper;
 import com.finreport.mapper.CFStatementMapper;
@@ -13,9 +18,9 @@ import com.finreport.model.CFStatement;
 import com.finreport.model.FinMainIndex;
 import com.finreport.model.IncStatement;
 import com.finreport.model.Stock;
-import com.sun.org.apache.bcel.internal.generic.RETURN;
 
 @Component
+@Transactional
 public class FinStatementDaoImpl implements FinStatementDao {
 	@Autowired
 	private BalSheetMapper balSheetMapper;
@@ -31,6 +36,9 @@ public class FinStatementDaoImpl implements FinStatementDao {
 	
 	@Autowired
 	private StockMapper stockMapper;
+	
+	@Autowired 
+	SqlSessionTemplate sqlBatchSessionTemplate;
 	
 	public BalSheet getBalanceByStockCode(String stockcode) {
 		return balSheetMapper.selectByPrimaryKey(stockcode);
@@ -59,5 +67,52 @@ public class FinStatementDaoImpl implements FinStatementDao {
 	public Boolean addStock(Stock stock) {
 		int result = stockMapper.insertSelective(stock);
 		return result > 0;
+	}
+	
+	public Boolean isStockExist(String symbol) {
+		return stockMapper.countBySymbol(symbol) > 0;
+	}
+	
+	public void batchAddBalStatement(List<BalSheet> balSheets) {
+		SqlSession sqlSession = sqlBatchSessionTemplate.getSqlSessionFactory().openSession();
+		BalSheetMapper balSheetMapper = sqlSession.getMapper(BalSheetMapper.class);
+		for (BalSheet balSheet : balSheets) {
+			balSheetMapper.insertSelective(balSheet);
+		}
+		
+		sqlSession.commit();
+	}
+	
+	public void batchAddIncStatement(List<IncStatement> incStatements) {
+		SqlSession sqlSession = sqlBatchSessionTemplate.getSqlSessionFactory().openSession();
+		
+		IncStatementMapper incStatementMapper = sqlSession.getMapper(IncStatementMapper.class);
+		for (IncStatement incStatement : incStatements) {
+			incStatementMapper.insertSelective(incStatement);
+		}
+		
+		sqlSession.commit();
+	}
+	
+	public void batchAddCFStatement(List<CFStatement> cfStatements) {
+		SqlSession sqlSession = sqlBatchSessionTemplate.getSqlSessionFactory().openSession();
+		
+		CFStatementMapper cfStatementMapper = sqlSession.getMapper(CFStatementMapper.class);
+		for (CFStatement cfStatement : cfStatements) {
+			cfStatementMapper.insertSelective(cfStatement);
+		}
+		
+		sqlSession.commit();
+	}
+	
+	public void batchAddFinMainIndex(List<FinMainIndex> finIndexs) {
+		SqlSession sqlSession = sqlBatchSessionTemplate.getSqlSessionFactory().openSession();
+		
+		FinMainIndexMapper finMainIndexMapper = sqlSession.getMapper(FinMainIndexMapper.class);
+		for (FinMainIndex item : finIndexs) {
+			finMainIndexMapper.insertSelective(item);
+		}
+		
+		sqlSession.commit();
 	}
 }
