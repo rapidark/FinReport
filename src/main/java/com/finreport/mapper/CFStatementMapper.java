a0632b6052cb32ba17932be3d7f6fab5f6786702
tuierbufei <52435770@qq.com>
2016-06-29 18:35:46 +0800

    Update CFStatementMapper.java

--- src/main/java/com/finreport/mapper/CFStatementMapper.java
+++ src/main/java/com/finreport/mapper/CFStatementMapper.java
@@ -1,13 +1,19 @@
-package com.finreport.mapper;
+package com.finreport.generator.mapper;
 
-import com.finreport.model.CFStatement;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.finreport.generator.model.CFStatement;
 
 public interface CFStatementMapper {
-    int deleteByPrimaryKey(String stockcode);
+    int deleteByPrimaryKey(@Param("stockcode") String stockcode, @Param("enddate") Integer enddate);
 
     int insert(CFStatement record);
 
     int insertSelective(CFStatement record);
 
-    CFStatement selectByPrimaryKey(String stockcode);
+    CFStatement selectByPrimaryKey(@Param("stockcode") String stockcode, @Param("enddate") Integer enddate);
+    
+    @Select("SELECT COUNT(*) from cash where stockcode = #{stockcode} and enddate=#{enddate}")
+    int countByPrimaryKey();
 }
