db2a33538ebf11bbcc4dfb0f119ecf028ab44b11
tuierbufei <52435770@qq.com>
2016-06-29 18:36:17 +0800

    Update FinMainIndexMapper.java

--- src/main/java/com/finreport/mapper/FinMainIndexMapper.java
+++ src/main/java/com/finreport/mapper/FinMainIndexMapper.java
@@ -1,13 +1,19 @@
-package com.finreport.mapper;
+package com.finreport.generator.mapper;
 
-import com.finreport.model.FinMainIndex;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import com.finreport.generator.model.FinMainIndex;
 
 public interface FinMainIndexMapper {
-    int deleteByPrimaryKey(String stockcode);
+    int deleteByPrimaryKey(@Param("stockcode") String stockcode, @Param("reportdate") Integer reportdate);
 
     int insert(FinMainIndex record);
 
     int insertSelective(FinMainIndex record);
 
-    FinMainIndex selectByPrimaryKey(String stockcode);
+    FinMainIndex selectByPrimaryKey(@Param("stockcode") String stockcode, @Param("reportdate") Integer reportdate);
+    
+    @Select("SELECT COUNT(*) from finindex where stockcode = #{stockcode} and enddate=#{enddate}")
+    int countByPrimaryKey();
 }
