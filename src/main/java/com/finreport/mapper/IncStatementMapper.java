c892706139be318021505018d6ea3e9f866babd3
tuierbufei <52435770@qq.com>
2016-06-29 18:39:18 +0800

    Update IncStatementMapper.java

--- src/main/java/com/finreport/mapper/IncStatementMapper.java
+++ src/main/java/com/finreport/mapper/IncStatementMapper.java
@@ -1,19 +1,19 @@
-package com.finreport.generator.mapper;
+package com.finreport.mapper;
 
 import org.apache.ibatis.annotations.Param;
 import org.apache.ibatis.annotations.Select;
 
-import com.finreport.generator.model.IncStatement;
+import com.finreport.model.IncStatement;
 
 public interface IncStatementMapper {
-    int deleteByPrimaryKey(@Param("stockcode") String stockcode, @Param("enddate") Integer enddate);
+	int deleteByPrimaryKey(@Param("stockcode") String stockcode, @Param("enddate") Integer enddate);
 
-    int insert(IncStatement record);
+	int insert(IncStatement record);
 
-    int insertSelective(IncStatement record);
+	int insertSelective(IncStatement record);
 
-    IncStatement selectByPrimaryKey(@Param("stockcode") String stockcode, @Param("enddate") Integer enddate);
-    
-    @Select("SELECT COUNT(*) from income where stockcode = #{stockcode} and enddate=#{enddate}")
-    int countByPrimaryKey();
+	IncStatement selectByPrimaryKey(@Param("stockcode") String stockcode, @Param("enddate") Integer enddate);
+	
+	@Select("SELECT COUNT(*) from income where stockcode = #{stockcode} and enddate=#{enddate}")
+    int countByPrimaryKey(String stockcode, Integer enddate);
 }
